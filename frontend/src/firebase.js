095430d29b26ffea0a34c7630058256cd5838411
19980410 <kkaoru1998@gmail.com>
2020-04-11 17:50:32 +0900

    makemoney

---
+++ frontend/src/firebase.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "aaaaaaaaaaaaa",
+  authDomain: "aaaaaaaaaaaa",
+  databaseURL:  "aaaaaaaaaaaa",
+  projectId: "teamc-before-after",
+  storageBucket: "teamc-before-after.appspot.com",
+  messagingSenderId: "aaaaaaaaaaaa",
+  appId:  "aaaaaaaaaaaa",
+  measurementId:  "aaaaaaaaaaaa"
+};
